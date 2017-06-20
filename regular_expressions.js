ce13701c83e4fbafd917a3de1b9eddacb1fed80b
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-06-20 19:22:54 +0700

    release 0 done

--- regular_expressions.js
+++ regular_expressions.js
@@ -3,6 +3,7 @@
 // Determine whether a string contains a nomor KTP
 const has_ktp = (string) => {
   // ...
+  return /\d{3}-\d{2}-\d{4}/.test(string);
 }
 
 console.log('has_ktp if it has what looks like a nomor KTP')
@@ -16,6 +17,16 @@ console.log(has_ktp('please confirm your identity: XXX-XX-1422') === false) // t
 // Return the Social Security number from a string.
 const grab_ktp = (string) => {
   // ...
+  let matchString = string.match(/\d{3}-\d{2}-\d{4}/);
+  if(matchString !== null)
+  {
+      return matchString[0];
+  }
+  else
+  {
+      return null;
+  }
+
 }
 
 console.log('grab_ktp returns nomor KTP if the string has an nomor KTP')
@@ -29,6 +40,14 @@ console.log(grab_ktp('please confirm your identity: XXX-XX-1422') === null) // t
 // Return all of the Social Security numbers from a string.
 const grab_all_nomor_ktp = (string) => {
   // ...
+  let matchString = string.match(/\d{3}-\d{2}-\d{4}/g);
+  if(matchString !== null)
+  {
+    return matchString;
+  }
+  else {
+    return [];
+  }
 }
 
 console.log('grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP')
@@ -42,6 +61,14 @@ console.log(grab_all_nomor_ktp('please confirm your identity: XXX-XX-1422')) // 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 const hide_all_nomor_ktp = (string) => {
   // ...
+  let matchString = string.replace(/(\d{3})-(\d{2})-(\d{4})/g, 'XXX-XXX-$3');
+  if(matchString !== null)
+  {
+    return matchString;
+  }
+  else {
+    return [];
+  }
 }
 
 console.log('hide_all_nomor_ktp obfuscates any nomor KTP in the string')
@@ -58,6 +85,19 @@ console.log(hide_all_nomor_ktp(hideString) === hideString) // true
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 const format_nomor = (string) => {
   // ...
+  let matchString = string.replace(/(\d{3})-(\d{2})-(\d{4})/g, '$1-$2-$3');
+
+  matchString = matchString.replace(/\./g, '-');
+  matchString = matchString.replace(/(\d{3})(\d{2})(\d{4})/, '$1-$2-$3');
+  if(matchString !== null)
+  {
+    return matchString;
+  }
+  else {
+    return [];
+  }
+
+
 }
 
 console.log('format_nomor finds and reformat any nomor KTP in the string')
